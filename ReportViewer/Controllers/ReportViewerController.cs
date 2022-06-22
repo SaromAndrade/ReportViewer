f26ff8d9809d9f902f84e1c240ef5675770aa5c0
Sarom Andrade <sarom.palmeiras@gmail.com>
2022-06-22 12:50:46 -0300

    Add project files.

---
+++ ReportViewer/Controllers/ReportViewerController.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using BoldReports.Web.ReportViewer;
+using System.Collections.Generic;
+
+namespace ReportViewer.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [Microsoft.AspNetCore.Cors.EnableCors("AllowAllOrigins")]
+    public class ReportViewerController : ControllerBase, IReportController
+    {
+        //Report Viewer requires a memory cache to store the information of consecutive client request and have the rendered report viewer information in server
+        private Microsoft.Extensions.Caching.Memory.IMemoryCache _cache;
+
+        // IWebHostEnvironment used to get the report stream from application `wwwroot\Resources` folder.
+        private Microsoft.AspNetCore.Hosting.IWebHostEnvironment _hostingEnvironment;
+
+        // Post action to process the report from server based json parameters and send the result back to the client.
+        public ReportViewerController(Microsoft.Extensions.Caching.Memory.IMemoryCache memoryCache,
+            Microsoft.AspNetCore.Hosting.IWebHostEnvironment hostingEnvironment)
+        {
+            _cache = memoryCache;
+            _hostingEnvironment = hostingEnvironment;
+        }
+        // Post action to process the report from server based json parameters and send the result back to the client.
+        [HttpPost]
+        public object PostReportAction([FromBody] Dictionary<string, object> jsonArray)
+        {
+            //Contains helper methods that help to process a Post or Get request from the Report Viewer control and return the response to the Report Viewer control
+            return ReportHelper.ProcessReport(jsonArray, this, this._cache);
+        }
+
+        [NonAction]
+        public void OnInitReportOptions(ReportViewerOptions reportOption)
+        {
+            string basePath = _hostingEnvironment.WebRootPath;
+            reportOption.ReportModel.ProcessingMode = ProcessingMode.Local;
+            // Here, we have loaded the sales-order-detail.rdl report from application the folder wwwroot\Resources. sales-order-detail.rdl should be there in wwwroot\Resources application folder.
+            FileStream inputStream = new FileStream(basePath + @"\Resources\Product List.rdlc", FileMode.Open, FileAccess.Read);
+            MemoryStream reportStream = new MemoryStream();
+            inputStream.CopyTo(reportStream);
+            reportStream.Position = 0;
+            inputStream.Close();
+            reportOption.ReportModel.Stream = reportStream;
+            reportOption.ReportModel.DataSources.Add(new BoldReports.Web.ReportDataSource { Name = "list", Value = ProductList.GetData() });
+        }
+
+        // Method will be called when reported is loaded with internally to start to layout process with ReportHelper.
+        [NonAction]
+        public void OnReportLoaded(ReportViewerOptions reportOption)
+        {
+        }
+
+        //Get action for getting resources from the report
+        [ActionName("GetResource")]
+        [AcceptVerbs("GET")]
+        // Method will be called from Report Viewer client to get the image src for Image report item.
+        public object GetResource(ReportResource resource)
+        {
+            return ReportHelper.GetResource(resource, this, _cache);
+        }
+
+        [HttpPost]
+        public object PostFormReportAction()
+        {
+            return ReportHelper.ProcessReport(null, this, _cache);
+        }
+    }
+}
